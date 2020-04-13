0e9b871f40f5aba299dcaf3ef855a7cc60a75471
mike-kob <37714867+mike-kob@users.noreply.github.com>
2020-04-13 12:45:16 +0300

    Updates

---
+++ precache-manifest.b19f6d33828ba9752241ae09e3584ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b978e83d6ff9c605d595dd0e95f85d6",
+    "url": "/YummyRecipe/index.html"
+  },
+  {
+    "revision": "8f1009890db02ffb4324",
+    "url": "/YummyRecipe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81eff6307a30d45b8029",
+    "url": "/YummyRecipe/static/js/2.e26aeca9.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/YummyRecipe/static/js/2.e26aeca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1009890db02ffb4324",
+    "url": "/YummyRecipe/static/js/main.88a825fa.chunk.js"
+  },
+  {
+    "revision": "90b68f0339470220997c",
+    "url": "/YummyRecipe/static/js/runtime-main.c0b0dc39.js"
+  }
+]);
