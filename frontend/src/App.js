31033e8aeeb8c75253e6d87b589562b75add439f
mike-kob <37714867+mike-kob@users.noreply.github.com>
2020-04-13 13:10:46 +0300

    Add user consent dialog and snackbar. Fix bugs with deleting recipe.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import { CssBaseline } from '@material-ui/core'
 
 import './App.css';
 import Router from './Router'
+import Snackbar from './components/Snackbar'
+import ConsentDialog from './components/ConsentDialog'
 
 function App() {
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <CssBaseline />
       <Router />
+      <Snackbar/>
+      <ConsentDialog />
     </div>
   );
 }
